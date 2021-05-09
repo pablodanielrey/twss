5ba237dd64af3a2691575ce88ded0f0ac3d59af4
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-05-08 21:11:55 -0300

    mas pruebas buscando datos sobre el grafo

--- entrega3/src/parse_jsons.py
+++ entrega3/src/parse_jsons.py
@@ -31,6 +31,15 @@ if __name__ == '__main__':
     g = Graph()
     g.parse(data=data1, format='json-ld', publicID='https://metacritic')
     
-    movie_type = URIRef('http://schema.org/Movie')
-    for m in g.triples((None,None,movie_type)):
-        print(m)
+    ''' listar todas las películas '''
+    movie = URIRef('http://schema.org/Movie')
+    for mid, _t, _m in g.triples((None,None,movie)):
+        for __m, t, o in g.triples((mid, None, None)):
+            print(t,o)
+
+    ''' listar solo los nombres de las películas '''
+
+    name = URIRef('http://schema.org/name')
+    for mid, _t, _m in g.triples((None,None,movie)):
+        for _m, __t, n in g.triples((mid, name, None)):
+            print(n)
