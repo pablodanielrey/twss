ffe3f6c2c2c99edf411642017fb588ee8feffc51
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-05-09 22:59:33 -0300

    agrego un ejemplo de como generar un frafo con los titulos de las pelis

--- entrega3/src/tp1_2_owl.py
+++ entrega3/src/tp1_2_owl.py
@@ -1,7 +1,8 @@
 import sys
 import json
+from urllib.parse import quote
 
-from rdflib import Graph, RDF, RDFS, Namespace
+from rdflib import Graph, RDF, RDFS, Namespace, Literal
 from rdflib.term import URIRef
 
 
@@ -11,9 +12,26 @@ def load_data():
     return data
 
 
+def get_ontology():
+    o = Namespace('https://raw.githubusercontent.com/pablodanielrey/twss/master/owl/twss_simple.ttl#')
+    d = Namespace('https://raw.githubusercontent.com/pablodanielrey/twss/master/owl/data/')
+    return (o,d)
 
 if __name__ == '__main__':
 
-    n = Namespace('https://raw.githubusercontent.com/pablodanielrey/twss/master/owl/twss_simple.ttl')
-    print(n)
-    
+    o,d = get_ontology()
+    g = Graph()
+    g.bind("twss", o)
+    g.bind("twssd", d)
+
+    data = load_data()
+    for movie in data['MOVIES']:
+        title = movie['TITLE']
+
+        print(title.replace(' ','_'))
+        vtitle = quote(title.replace(' ','_'))
+        dmovie = d[vtitle]
+        g.add((dmovie, RDF.type, o.Movie))
+        g.add((dmovie, o.title, Literal(title)))
+
+    print(g.serialize(format="turtle").decode("utf-8"))
