a24eee9531598875510fee4998c3f09697987068
Pablo Daniel Rey <pablodanielrey@gmail.com>
2021-05-13 17:23:25 -0300

    esta funcionando el script de obtener el turtle a partir de la url /resource/

--- entrega4/src/process_redirections_to_turtle.py
+++ entrega4/src/process_redirections_to_turtle.py
@@ -3,6 +3,34 @@ import json
 import requests
 import re
 
+def get_content(req):
+
+    if (req.status_code >= 300 and req.status_code < 400):
+        ''' es una redirección '''
+        url = req.headers['Location']
+        assert url != None
+        req = requests.get(url, headers={'Accept':'text/turtle'}, allow_redirects=False)
+        return get_content(req)
+
+    if req.status_code == 200:
+        return req.text
+
+    return None
+    """
+        esto por lo que veo no es necesario 
+    if content and req.status_code != 200:
+        ''' analizo el contenido alternativo '''
+        alternates = r.headers.get('Alternates',None)
+        if not alternates:
+            print(f'No existe representación text/turtle para la url {url}')
+            return None
+        url = process_alternates(alternates)
+
+
+    r = requests.get(url, headers={'Accept':'text/turtle'}, allow_redirects=False)
+    return url
+    """
+
 
 def process_alternates(alternates):
     reg = re.compile('{\"(.*)\".*?{type (.*?)}}')
@@ -19,11 +47,5 @@ def process_alternates(alternates):
 if __name__ == '__main__':
 
     url = sys.argv[1]
-    r = requests.get(url, headers={'Accept':'text/turtle'}, allow_redirects=False)
-    alternates = r.headers.get('Alternates',None)
-    if not alternates:
-        print(f'No existe representación text/turtle para la url {url}')
-        sys.exit(1)
-    url = process_alternates(alternates)
-    print(url)
-
+    r = requests.get(url, headers={'Accept':'text/turtle'}, allow_redirects=True)
+    print(get_content(r))
